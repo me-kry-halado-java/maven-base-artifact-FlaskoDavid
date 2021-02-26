0525d24c1ed0c8860b3ace5fc06731f2efa1777b
flasko <flasko.david@gmail.com>
2021-02-26 13:21:01 +0100

    space hiany kezeles

--- src/main/java/hu/meiit/haladojava/calculator/Function.java
+++ src/main/java/hu/meiit/haladojava/calculator/Function.java
@@ -1,10 +1,37 @@
 package hu.meiit.haladojava.calculator;
 
+import java.util.ArrayList;
+
 public class Function {
     private static final String errorResponse="-";
     public static String[] parse(String expression){
+        if(expression.contains(" ")){
+            return expression.split(" ");
+        }
+        char sub;
+        ArrayList<String> express=new ArrayList<String>();
+        String[]other;
+        String[] retvalue=new String[3];
+        int index=0;
+        if (expression.contains("+")){
+            index=expression.indexOf("+");
+        }else if(expression.contains("-")){
+            index=expression.indexOf("-");
+        }else if(expression.contains("*")){
+            index=expression.indexOf("*");
+        }else if(expression.contains("/")){
+            index=expression.indexOf("/");
+        }
+        sub=expression.charAt(index);
 
-        return expression.split(" ");
+        other=expression.split("[+ - * /]");
+        express.add(other[0]);
+        express.add(String.valueOf(sub));
+        express.add(other[1]);
+        for (int i=0;i<express.size();i++){
+            retvalue[i]=express.get(i);
+        }
+        return retvalue;
     }
     public static Expression getAsExpression(String[] parts){
         return new Expression(
